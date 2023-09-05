5d1c93179ff6177b71453f9225885d264eecde9c
hoodiboiy <112661257+hoodiboiy@users.noreply.github.com>
2023-09-05 03:27:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-vg5wj6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-vg5wj6)
---
+++ index.js
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <h1>Welcome to Our Hospital</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Doctors</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section id="content">
+        <h2>Our Services</h2>
+        <p>We offer a wide range of medical services to meet your healthcare needs.</p>
+        <ul>
+            <li>Emergency Care</li>
+            <li>Cardiology</li>
+            <li>Orthopedics</li>
+            <li>Obstetrics</li>
+        </ul>
+    </section>
+
+    <section id="doctors">
+        <h2>Our Doctors</h2>
+        <div class="doctor">
+            <img src="doctor1.jpg" alt="Doctor 1">
+            <h3>Dr. John Smith</h3>
+            <p>Cardiologist</p>
+        </div>
+        <div class="doctor">
+            <img src="doctor2.jpg" alt="Doctor 2">
+            <h3>Dr. Sarah Johnson</h3>
+            <p>Orthopedic Surgeon</p>
+        </div>
+    </section>
+
+    <section id="contact">
+        <h2>Contact Us</h2>
+        <p>If you have any questions or need to schedule an appointment, please contact us:</p>
+        <address>
+            Email: <a href="mailto:info@hospital.com">info@hospital.com</a><br>
+            Phone: <a href="tel:+123456789">+123 456 789</a>
+        </address>
+    </section>
+</body>
+</html>
